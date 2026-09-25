bbc062817f7cdab1fde473347e8f435531abffa1
JustMili <rodx160@gmail.com>
2026-09-25 17:58:30 +0200

    Updates people updatess

--- common/src/main/java/net/justmili/shulkersinbundles/core/mixin/BundleItemMixin.java
+++ common/src/main/java/net/justmili/shulkersinbundles/core/mixin/BundleItemMixin.java
@@ -3,6 +3,7 @@ package net.justmili.shulkersinbundles.core.mixin;
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.justmili.shulkersinbundles.core.util.Util;
 import net.minecraft.world.entity.SlotAccess;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
@@ -16,11 +17,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(BundleItem.class)
 public abstract class BundleItemMixin {
 
-    // Prevent inserting items into Bundles that are already in a Shulker Box
+    // Prevent inserting Shulker Boxes and Bundles into Bundles that are already in a Shulker Box
     @Definition(id = "allowModification", method = "Lnet/minecraft/world/inventory/Slot;allowModification(Lnet/minecraft/world/entity/player/Player;)Z")
     @Expression("?.allowModification(?)")
     @ModifyExpressionValue(method = "overrideOtherStackedOnMe", at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 0))
     private boolean sib$preventModification(boolean original, ItemStack self, ItemStack other, Slot slot, ClickAction click, Player player, SlotAccess carriedItem) {
-        return !(slot instanceof ShulkerBoxSlot) && original;
+        return !(slot instanceof ShulkerBoxSlot && Util.isShulkerOrBundle(other.getItem())) && original;
     }
 }
--- common/src/main/java/net/justmili/shulkersinbundles/core/util/Util.java
+++ common/src/main/java/net/justmili/shulkersinbundles/core/util/Util.java
@@ -1,7 +1,9 @@
 package net.justmili.shulkersinbundles.core.util;
 
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.BundleItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.BundleContents;
@@ -9,16 +11,29 @@ import net.minecraft.world.level.block.ShulkerBoxBlock;
 
 public class Util {
 
-    public static boolean isBundleAndEmpty(ItemStack stack) {
-        return stack.getOrDefault(DataComponents.BUNDLE_CONTENTS, BundleContents.EMPTY).equals(BundleContents.EMPTY);
+    public static boolean isBundleWithAllowed(ItemStack stack) {
+        var contents = stack.getOrDefault(DataComponents.BUNDLE_CONTENTS, BundleContents.EMPTY);
+        boolean allowInteract = true;
+
+        for (var item : contents.items()) {
+            if (item.is(ItemTags.BUNDLES) || item.is(ItemTags.SHULKER_BOXES)) {
+                allowInteract = false;
+                break;
+            }
+        }
+        return contents.isEmpty() || allowInteract;
     }
 
     public static boolean preventInteract(ItemStack stack, boolean original) {
-        return Util.isBundleAndEmpty(stack) && original;
+        return Util.isBundleWithAllowed(stack) && original;
     }
 
     public static boolean allowInteract(ItemStack stack, boolean original) {
-        return Util.isBundleAndEmpty(stack) || original;
+        return Util.isBundleWithAllowed(stack) || original;
+    }
+
+    public static boolean isShulkerOrBundle(Item item) {
+        return isShulkerBox(item) || item instanceof BundleItem;
     }
 
     public static boolean isShulkerBox(Item item) {
